073b887f463a0027343a656165b47b642e8c8162
hahwul <hahwul@gmail.com>
2021-02-01 22:28:28 +0900

    update

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func main(){
 			}
 		}
 	} else {
-		target, err := readLinesOrLiteral(*list)
+		target, err := module.ReadLinesOrLiteral(*list)
 		_ = err
 		for i, v := range target {
 			_ = i
@@ -41,40 +41,4 @@ func checkURL(s string) {
 	} else if size == 5 {
 		fmt.Println(s)
 	}
-}
-
-func readLines(filename string) ([]string, error) {
-	f, err := os.Open(filename)
-	if err != nil {
-		return []string{}, err
-	}
-	defer f.Close()
-
-	lines := make([]string, 0)
-	sc := bufio.NewScanner(f)
-	for sc.Scan() {
-		lines = append(lines, sc.Text())
-	}
-
-	return lines, sc.Err()
-}
-
-// readLinesOrLiteral tries to read lines from a file, returning
-// the arg in a string slice if the file doesn't exist, unless
-// the arg matches its default value
-func readLinesOrLiteral(arg string) ([]string, error) {
-	if isFile(arg) {
-		return readLines(arg)
-	}
-
-	// if the argument isn't a file, but it is the default, don't
-	// treat it as a literal value
-
-	return []string{arg}, nil
-}
-
-// isFile returns true if its argument is a regular file
-func isFile(path string) bool {
-	f, err := os.Stat(path)
-	return err == nil && f.Mode().IsRegular()
 }
